fe72471a9a3fa209cabf09da632c0b5015056fb8
TheAustinMiller <themilleraustin@gmail.com>
2026-01-21 19:51:43 -0600

    Begin adding Kanye Songs

--- src/album-data.ts
+++ src/album-data.ts
@@ -1,33 +1,57 @@
 export interface Album {
-  title: string;
-  artist: string;
-  coverArt: string;
-  collectionKey: string; // This links to your MY_COLLECTIONS keys
+    title: string;
+    artist: string;
+    coverArt: string;
+    collectionKey: string; // This links to your MY_COLLECTIONS keys
 }
 
 export const ALBUMS: Album[] = [
-  {
-    title: 'I Hope You\'re Happy',
-    artist: 'BigXthaPlug',
-    coverArt: 'https://upload.wikimedia.org/wikipedia/en/thumb/3/3e/BigXthaPlug_-_I_Hope_You%27re_Happy.jpg/250px-BigXthaPlug_-_I_Hope_You%27re_Happy.jpg',
-    collectionKey: 'IHopeYoureHappy'
-  },
-  {
-    title: 'Take Care',
-    artist: 'BigXthaPlug',
-    coverArt: 'https://upload.wikimedia.org/wikipedia/en/4/40/BigXthaPlug_-_Take_Care.png',
-    collectionKey: 'TakeCare'
-  },
-  {
-    title: 'The Biggest',
-    artist: 'BigXthaPlug',
-    coverArt: 'https://i.scdn.co/image/ab67616d0000b2739afbbc313ea008b68cc5f40d',
-    collectionKey: 'TheBiggest'
-  },
-  {
-    title: 'AMAR',
-    artist: 'BigXthaPlug',
-    coverArt: 'https://upload.wikimedia.org/wikipedia/en/7/7e/BigXthaPlug_-_Amar.jpg',
-    collectionKey: 'Amar'
-  }
+    {
+        title: 'I Hope You\'re Happy',
+        artist: 'BigXthaPlug',
+        coverArt: 'https://upload.wikimedia.org/wikipedia/en/thumb/3/3e/BigXthaPlug_-_I_Hope_You%27re_Happy.jpg/250px-BigXthaPlug_-_I_Hope_You%27re_Happy.jpg',
+        collectionKey: 'IHopeYoureHappy'
+    },
+    {
+        title: 'Take Care',
+        artist: 'BigXthaPlug',
+        coverArt: 'https://upload.wikimedia.org/wikipedia/en/4/40/BigXthaPlug_-_Take_Care.png',
+        collectionKey: 'TakeCare'
+    },
+    {
+        title: 'The Biggest',
+        artist: 'BigXthaPlug',
+        coverArt: 'https://i.scdn.co/image/ab67616d0000b2739afbbc313ea008b68cc5f40d',
+        collectionKey: 'TheBiggest'
+    },
+    {
+        title: 'AMAR',
+        artist: 'BigXthaPlug',
+        coverArt: 'https://upload.wikimedia.org/wikipedia/en/7/7e/BigXthaPlug_-_Amar.jpg',
+        collectionKey: 'Amar'
+    },
+    {
+        title: 'The College Dropout',
+        artist: 'Kanye West',
+        coverArt: 'https://upload.wikimedia.org/wikipedia/en/a/a3/Kanyewest_collegedropout.jpg',
+        collectionKey: 'CollegeDropout'
+    },
+    {
+        title: 'Late Registration',
+        artist: 'Kanye West',
+        coverArt: 'https://upload.wikimedia.org/wikipedia/en/f/f4/Late_registration_cd_cover.jpg',
+        collectionKey: 'LateRegistration'
+    },
+    {
+        title: 'Graduation',
+        artist: 'Kanye West',
+        coverArt: 'https://upload.wikimedia.org/wikipedia/en/7/70/Graduation_%28album%29.jpg',
+        collectionKey: 'Graduation'
+    },
+    {
+        title: '808s & Heartbreak',
+        artist: 'Kanye West',
+        coverArt: 'https://upload.wikimedia.org/wikipedia/en/f/f1/808s_%26_Heartbreak.png',
+        collectionKey: '808sHeartbreak'
+    }
 ];
